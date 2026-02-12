c0f333085edf2f553145ad9c5c5b3f25dc70c227
w2sv <zangenbergjanek@googlemail.com>
2026-02-12 20:13:04 +0100

    refactor: BaseConfig.kt

--- build-logic/convention/src/main/kotlin/helpers/BaseConfig.kt
+++ build-logic/convention/src/main/kotlin/helpers/BaseConfig.kt
@@ -4,68 +4,69 @@ import com.android.build.api.dsl.ApplicationDefaultConfig
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
-internal sealed interface Namespace {
-    object Auto : Namespace
-
-    @JvmInline
-    value class Manual(val namespace: String) : Namespace
-}
-
 internal fun Project.applyBaseConfig(excludeMetaInfResources: Boolean = true, namespace: Namespace = Namespace.Auto) {
     pluginManager.applyPlugins("ktlint", catalog = catalog)
+
+    extensions.configureKotlinAndroid()
+    extensions.configureCommon(
+        namespace = namespace.get(path),
+        catalog = catalog,
+        excludeMetaInfResources = excludeMetaInfResources
+    )
+
     setRobolectricSdk(this)
+}
 
-    extensions.apply {
-        configure<KotlinAndroidProjectExtension> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_17)
-                freeCompilerArgs.addAll(
-                    "-opt-in=kotlinx.coroutines.ExperimentalForInheritanceCoroutinesApi",
-                    "-Xannotation-default-target=param-property",
-                    "-Xwhen-guards",
-                    "-XXLanguage:+ExplicitBackingFields",
-                    "-Xnested-type-aliases",
-                    "-Xcontext-sensitive-resolution",
-                    "-Xcontext-parameters"
-                )
-            }
+private fun ExtensionContainer.configureKotlinAndroid() {
+    configure<KotlinAndroidProjectExtension> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.addAll(
+                "-opt-in=kotlinx.coroutines.ExperimentalForInheritanceCoroutinesApi",
+                "-Xannotation-default-target=param-property",
+                "-Xwhen-guards",
+                "-XXLanguage:+ExplicitBackingFields",
+                "-Xnested-type-aliases",
+                "-Xcontext-sensitive-resolution",
+                "-Xcontext-parameters"
+            )
         }
-        configure<CommonExtension> {
-            this.namespace = when (namespace) {
-                is Namespace.Auto -> "com.w2sv." + path
-                    .removePrefix(":")
-                    .replace(':', '.')
-                    .replace('-', '_')  // Sets namespace to "com.w2sv.<module-name>"
-                is Namespace.Manual -> namespace.namespace
-            }
+    }
+}
 
-            defaultConfig.apply {
-                minSdk = catalog.findVersionInt("minSdk")
-                compileSdk = catalog.findVersionInt("compileSdk")
-                if (this is ApplicationDefaultConfig) {
-                    targetSdk = catalog.findVersionInt("compileSdk")
-                }
-                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+private fun ExtensionContainer.configureCommon(
+    namespace: String,
+    catalog: VersionCatalog,
+    excludeMetaInfResources: Boolean
+) {
+    configure<CommonExtension> {
+        this.namespace = namespace
+        defaultConfig.apply {
+            minSdk = catalog.findVersionInt("minSdk")
+            compileSdk = catalog.findVersionInt("compileSdk")
+            (this as? ApplicationDefaultConfig)?.run { targetSdk = catalog.findVersionInt("compileSdk") }
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+        compileOptions.apply {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+        testOptions.apply {
+            unitTests {
+                isReturnDefaultValues = true
+                isIncludeAndroidResources = true
+                all { test -> test.failOnNoDiscoveredTests.set(false) }
             }
-            compileOptions.apply {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-            testOptions.apply {
-                unitTests {
-                    isReturnDefaultValues = true
-                    isIncludeAndroidResources = true
-                    all { test -> test.failOnNoDiscoveredTests.set(false) }
-                }
-                animationsDisabled = true
-            }
-            if (excludeMetaInfResources) {
-                packaging.resources.excludes.add("/META-INF/*")
-            }
+            animationsDisabled = true
+        }
+        if (excludeMetaInfResources) {
+            packaging.resources.excludes.add("/META-INF/*")
         }
     }
 }
